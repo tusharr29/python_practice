ef5d45b8d75eb23e978e5107fcc300c42df123a6
Tushar Deshmukh <tusharsd2905@gmail.com>
2026-01-23 19:42:42 +0530

    For Loop Pattern

---
+++ for_loops/q_1.py
@@ -0,0 +1,11 @@
+'''Question 1: Print following pattern using for loops
+    *****
+    *****
+    *****
+    *****
+    *****'''
+
+for i in range(1,6):
+    for j in range(1,6):
+        print("*", end=" ")
+    print()
